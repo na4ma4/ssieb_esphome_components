9f109a779f374a9206b7945f5c894034d95fdade
na4ma4 <na4ma4@users.noreply.github.com>
2025-08-01 17:24:08 +1000

    fixup! fix(ht16k33_alpha): fixing the brightness methods not working

--- components/ht16k33_alpha/ht16k33_display.cpp
+++ components/ht16k33_alpha/ht16k33_display.cpp
@@ -97,8 +97,8 @@ void HT16K33AlphaDisplay::set_brightness(float level) {
     if (val == 0) {
       display->write_bytes(DISPLAY_COMMAND_DISPLAY_OFF, nullptr, 0);
     } else {
-      display->write_bytes(DISPLAY_COMMAND_DIMMING | (val - 1), nullptr, 0);
       display->write_bytes(DISPLAY_COMMAND_DISPLAY_ON, nullptr, 0);
+      display->write_bytes(DISPLAY_COMMAND_DIMMING | (val - 1), nullptr, 0);
     }
   }
 }
